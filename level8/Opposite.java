fb091a12b1a3b074b7a2d1f061bd77234d827c4a
Michael Cataldo <cataldmj194@potsdam.edu>
2017-06-12 11:23:38 -0400

    Completed Opposite.java

---
+++ level8/Opposite.java
@@ -0,0 +1,11 @@
+/*
+  Name: Michael Cataldo
+  Assignment: Opposite
+  Date: 6/11/2017
+*/
+
+public class Kata {
+  public static int opposite(int number) {
+    return -number;           
+  }
+}
