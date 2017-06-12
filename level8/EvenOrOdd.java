46a5b573efb6deb9c62540f62566360e0a4ceded
Michael Cataldo <cataldmj194@potsdam.edu>
2017-06-12 11:15:16 -0400

    Completed EvenOrOdd.java

---
+++ level8/EvenOrOdd.java
@@ -0,0 +1,11 @@
+/** 
+   Name: Michael Cataldo
+   Assignment: Even or Odd
+   Date: 6/11/2017
+**/
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        return (number%2 == 0) ? "Even" : "Odd";
+    }
+}
